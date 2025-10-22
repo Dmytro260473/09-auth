bfad8733bed77c616c9e2b4900d765d3483fe8f2
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-10-22 22:50:37 +0300

    hw

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,19 +1,19 @@
+
 import NoteListClient from "./Notes.client";
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchServerNotes } from "@/lib/api/serverApi";
 import { Metadata } from "next";
 
 type Props = {
-  params: Promise<{ slug: string[] }>;
+  params: { slug: string[] };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = await params;
-  const tag = slug[0];
+  const tag = await params.slug[0];
 
   const title = `Notes filtered by: ${tag}`;
   const description = `Review of notes filtered by "${tag}".`;
-  const url = `https://09-auth-six-zeta.vercel.app/notes/filter/${tag}`;
+  const url = `https://09-auth-six-zeta.vercel.app/notes/filter/${params.slug[0]}`;
 
   return {
     title,
@@ -37,21 +37,98 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function App({ params }: Props) {
-  const { slug } = await params;
-  const tag = slug[0] === "All" ? undefined : slug[0];
+  const queryClient = new QueryClient()
+	const { slug } = await params
+	const tag = slug[0] === "All" ? undefined : slug[0]
 
-  const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
-    queryKey: ['notes', { query: "", page: 1, tag }],
-    queryFn: () => fetchServerNotes(1, "", tag),
+    queryKey: ['notes', { query: "", page: 1, tag: tag }],
+    queryFn: () => fetchServerNotes(1, "", tag ),
   });
 
   return (
     <div>
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <NoteListClient tag={tag} />
+        <NoteListClient tag={tag}   />
       </HydrationBoundary>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import css from "./notesPage.module.css";
+// import NoteListClient from "./Notes.client";
+// import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
+// import { fetchServerNoteById } from "@/lib/api/serverApi";
+// import { Metadata } from "next";
+
+
+// type Props = {
+//   params: Promise<{ slug: string[] }>;
+// };
+
+
+// export async function generateMetadata({ params }: Props): Promise<Metadata>  {
+//   const {slug} = await params;
+//   const tag = slug[0];
+  
+  
+//   const title = `Notes filtered by: ${tag}`;
+//   const description = `Review of notes filtered by "${tag}".`;
+//   const url = `https://09-auth-six-zeta.vercel.app/notes/filter/${slug[0]}`;
+
+//   return {
+//     title,
+//     description,
+//     openGraph: { 
+//       title,
+//       description,
+//       url,
+//       siteName: 'NoteHub',
+//       images: [
+//        {
+//         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//                 width: 1200,
+//         height: 630,
+//         alt: "Title",
+//       },
+//       ],
+//       type: 'website',
+//     },
+//   };
+// }
+ 
+
+// export default async function App({ params }: Props) {
+//   const queryClient = new QueryClient();
+//   const {slug} = await params;
+//   const tag = slug[0] === 'All' ? undefined : slug[0];
+
+//   queryClient.prefetchQuery({
+//     queryKey: ['notes', {query: "", page: 1, tag: tag}],
+//     queryFn: () => fetchServerNoteById(1, "", tag),
+//   });
+//   return (
+//     <div className={css.app}>
+//       <HydrationBoundary state={dehydrate(queryClient)}>
+//         <NoteListClient tag={tag}/>
+//       </HydrationBoundary>
+//     </div>
+//   );
+// }
--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -7,73 +7,72 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuthStore } from '@/lib/store/authStore';
 
-const EditProfile = () => {
-  const setUser = useAuthStore((state) => state.setUser);
-  const [userEmail, setUserEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [avatarUrl, setAvatarUrl] = useState('');
-  const router = useRouter();
+const EditProfile =  () => {
+    const setUser = useAuthStore((state) => state.setUser);
+    const [userEmail, setUserEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const router = useRouter();
 
-  useEffect(() => {
-    getMe().then(user => {
-      if (user) {
-        setUserEmail(user.email);
-        setUsername(user.username);
-        setAvatarUrl(user.avatar);
-      }
-    });
-  }, []);
+    useEffect(() => {
+        getMe().then(user => {
+          setUserEmail(user.email);
+          setUsername(user.username);
+          setAvatarUrl(user.avatar);
+        });
+    }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  }
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const updatedUser = await updateMe({ username });
-      setUser(updatedUser);
-      router.push('/profile'); // После сохранения остаём переход на профиль
-    } catch (error) {
-      console.log('Error updating profile:', error);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setUsername(e.target.value)
     }
-  }
 
-  return (
-    <main className={css.mainContent}>
-      <div className={css.profileCard}>
-        <h1 className={css.formTitle}>Edit Profile</h1>
+    const handleSubmit =async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        await updateMe({username});
+        setUser({username, email: userEmail, avatar: avatarUrl});
+        router.push('/profile');
+      } catch (error) {
+        console.log('Error updating profile:', error);
+      }
+    }
 
-        {avatarUrl && (
-          <Image src={avatarUrl} alt="User Avatar" width={120} height={120} className={css.avatar} />
-        )}
+    return (<main className={css.mainContent}>
+  <div className={css.profileCard}>
+    <h1 className={css.formTitle}>Edit Profile</h1>
 
-        <form className={css.profileInfo} onSubmit={handleSubmit}>
-          <div className={css.usernameWrapper}>
-            <label htmlFor="username">Username:</label>
-            <input
-              id="username"
-              type="text"
-              className={css.input}
-              value={username}
-              onChange={handleChange}
-            />
-          </div>
-
-          <p>Email: {userEmail}</p>
-
-          <div className={css.actions}>
-            <button type="submit" className={css.saveButton}>
-              Save
-            </button>
-            <button type="button" className={css.cancelButton} onClick={() => router.back()}>
-              Cancel
-            </button>
-          </div>
-        </form>
+    {avatarUrl && <Image src={avatarUrl}
+      alt="User Avatar"
+      width={120}
+      height={120}
+      className={css.avatar}
+    />
+}
+    <form className={css.profileInfo} onSubmit={handleSubmit}>
+      <div className={css.usernameWrapper}>
+        <label htmlFor="username">Username:</label>
+        <input id="username"
+          type="text"
+          className={css.input}
+          value={username}
+          onChange={handleChange}
+        />
       </div>
-    </main>
-  )
+
+      <p>Email: {userEmail}</p>
+
+      <div className={css.actions}>
+        <button type="submit" className={css.saveButton}>
+          Save
+        </button>
+        <button type="button" className={css.cancelButton} onClick={() => router.push('/profile')}  >
+          Cancel
+        </button>
+      </div>
+    </form>
+  </div>
+</main>
+)
 }
 
 export default EditProfile;
